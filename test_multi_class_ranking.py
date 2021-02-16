6f9ad7455ae5f4de5f4964e7a68248ba10a08de4
Shapour Nemati <shapour.nemati@studio.unibo.it>
2021-02-16 14:36:17 +0100

    test: compute and save descriptors

--- test_multi_class_ranking.py
+++ test_multi_class_ranking.py
@@ -1,9 +1,16 @@
 import cv2 as cv
+import numpy as np
 
-from multi_class_ranking import dense_sampling_points
+from multi_class_ranking import dense_sampling_points, compute_and_save_descriptors
 
 def test_dense_sampling_points():
     img = cv.imread("8159.JPG")
     dense_sampling_points(img)
 
-test_dense_sampling_points()
+def test_compute_and_save_descriptors():
+    img1 = cv.imread("8159.JPG")
+    img2 = cv.imread("8159.JPG")
+    filename = "prova"
+    des = compute_and_save_descriptors([img1, img2], filename)
+    des2 = np.load(filename + ".npy")
+    assert np.array_equal(des, des2) 
