98b7059272decb71c2439814f56d8d121cbf6ed0
Shapour Nemati <shapour.nemati@studio.unibo.it>
2021-02-17 17:03:18 +0100

    test: classify with given histogram and model

--- test_multi_class_ranking.py
+++ test_multi_class_ranking.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-from multi_class_ranking import dense_sampling_points, compute_descriptors, compute_codebook
+from multi_class_ranking import dense_sampling_points, compute_descriptors, compute_codebook, compute_histogram, classify
 
 def test_dense_sampling_points():
     img = cv.imread("8159.JPG")
@@ -15,3 +15,11 @@ def test_compute_descriptors():
 def test_compute_codebook():
     descriptors = np.load("sample_descriptors.npy")
     vocabulary = compute_codebook(descriptors)
+
+def test_classify():
+    vocabulary = np.load("vocabulary.npy")
+    model = np.load("model.npy")
+    img = cv.imread("images\\web1.jpg")
+    descriptors = compute_descriptors(img)
+    histogram = compute_histogram(descriptors, vocabulary)
+    assert classify(histogram, model) == 53
