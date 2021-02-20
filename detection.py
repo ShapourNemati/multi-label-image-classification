1637d1d9424dfa61a59cd8678bcc98c081e71d9d
Shapour Nemati <shapour.nemati@studio.unibo.it>
2021-02-20 13:34:06 +0100

    feat: sliding window detection

--- detection.py
+++ detection.py
@@ -1,3 +1,5 @@
+import sys
+
 import cv2 as cv
 import numpy as np
 
@@ -18,7 +20,30 @@ def descriptors_matrix(img):
 def subregion_descriptors(descriptors_matrix, x, y, width, height):
     step = 16
     descriptors = []
-    for i in range(x, width, step):
-        for j in range (y, height, step):
-            descriptors.append(descriptors_matrix[int(i/step)][int(j/step)])
+    for i in range(x, width + x, step):
+        for j in range (y, height + y, step):
+            descriptors.append(descriptors_matrix[int(j/step)][int(i/step)])
     return descriptors
+
+def sliding_window_detection(template, img):
+    step = 16
+    template = cv.resize(template, (200, 200))
+    img = cv.resize(img, (600, 450))
+    template_width, template_height, _ = template.shape
+    img_width, img_height, _ = img.shape
+    desc_mat = descriptors_matrix(img)
+    min_dist = sys.float_info.max
+    min_upper_corner = (0, 0)
+    template_descriptors = subregion_descriptors(descriptors_matrix(template), 0, 0, template_width, template_height)
+    for i in range(0, img_height - template_height, step):
+        for j in range(0, img_width - template_width, step):
+            sub_desc = subregion_descriptors(desc_mat, j, i, template_width, template_height)
+            dist = 0
+            for z in range(len(sub_desc)):
+                dist = dist + np.linalg.norm(sub_desc[z] - template_descriptors[z])
+            mean_dist = dist / len(sub_desc)
+            if mean_dist < min_dist:
+                min_dist = mean_dist
+                min_upper_corner = (i, j)
+    return (min_dist, min_upper_corner)
+
