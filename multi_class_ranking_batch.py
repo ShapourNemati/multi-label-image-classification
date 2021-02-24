167c06be353b45a8f6e3d02990b79bf874657dc3
Shapour Nemati <shapour.nemati@studio.unibo.it>
2021-02-24 13:12:54 +0100

    feat: compute the mean average precision

--- multi_class_ranking_batch.py
+++ multi_class_ranking_batch.py
@@ -10,24 +10,54 @@ from detection import descriptors_matrix, subregion_descriptors, sliding_window_
 
 folder_name = "test\\"
 jpg_filenames = glob.glob(folder_name + "*.jpg")
+metadata_filenames = [f for f in glob.glob(folder_name + "*.npy") if "_ranking" not in f]
 
-vocabulary = np.load("vocabulary.npy")
-model = np.load("model.npy")
+#STEP 1 - Compute and save multi-class-ranking
 
-for i in range(0, len(jpg_filenames), 15):
-    image_file = jpg_filenames[i]
+# vocabulary = np.load("vocabulary.npy")
+# model = np.load("model.npy")
 
-    classes_votes = []
-    test = cv.imread(image_file)
-    grids = divide_in_grids(test)
-    for grid in grids:
-        descriptors = compute_descriptors(grid)
-        histogram = compute_histogram(descriptors, vocabulary)
-        classes_votes.append(classify(histogram, model))
+# for i in range(0, len(jpg_filenames), 15):
+#     image_file = jpg_filenames[i]
 
-    sorted_list = sorted(classes_votes, key = classes_votes.count, reverse = True)
-    ranking = list(dict.fromkeys(sorted_list))
+#     classes_votes = []
+#     test = cv.imread(image_file)
+#     grids = divide_in_grids(test)
+#     for grid in grids:
+#         descriptors = compute_descriptors(grid)
+#         histogram = compute_histogram(descriptors, vocabulary)
+#         classes_votes.append(classify(histogram, model))
+
+#     sorted_list = sorted(classes_votes, key = classes_votes.count, reverse = True)
+#     ranking = list(dict.fromkeys(sorted_list))
     
-    save_descriptors(ranking, image_file[:-4] + "_ranking")
+#     save_descriptors(ranking, image_file[:-4] + "_ranking")
 
-    print ("saved " + str(i))
+#     print ("saved " + str(i))
+
+#STEP 2 - Calculate mAP
+
+average_precision = []
+max_k = 50
+
+for i in range(0, len(metadata_filenames), 15):
+    metadata_file = metadata_filenames[i]
+    ranking_file = metadata_file[:-4] + "_ranking.npy"
+
+    metadata = np.load(metadata_file)
+    ranking = np.load(ranking_file)
+
+    relevant_items = (metadata[:, 5]).flatten()
+    relevant_items = set(relevant_items)
+
+    precision = []
+
+    for k in range(1, max_k + 1):
+        if k < len(ranking):
+            considered_votes = ranking[:k].flatten()
+            true_positives = list(set(considered_votes) & relevant_items)
+            precision.append(len(true_positives) / len(considered_votes))
+
+    average_precision.append(sum(precision) / len(precision))
+
+print(sum(average_precision) / len(average_precision))
